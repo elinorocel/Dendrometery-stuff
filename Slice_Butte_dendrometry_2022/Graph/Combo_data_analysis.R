ff5807c6ba5aeeafcef28a724d6ea0bf23e36505
elinorocel <132934625+elinorocel@users.noreply.github.com>
2023-05-09 17:04:47 -0700

    graphed stuff

--- Slice_Butte_dendrometry_2022/Graph/Combo_data_analysis.R
+++ Slice_Butte_dendrometry_2022/Graph/Combo_data_analysis.R
@@ -4,7 +4,7 @@ d133 <- read.csv("Cleaning/dendro_133_cleaned.csv")
 d133 <- d133 %>% mutate(ts = ymd_hms(ts))
 
 d140 <- read.csv("Cleaning/dendro_140_cleaned.csv")
-d140 <- d140 %>% mutate
+d140 <- d140 %>% mutate(ts = ymd_hms(ts))
 
 
 #subset dendro 133 to equalize
@@ -13,7 +13,11 @@ d133 <- d133[-1,]
 str(d140)
 
 combined_dendro <- cbind(d133,d140)
-
+#try plotting combined (unsuccessful)
+ggplot((subset(combined_dendro, ts < as.POSIXct("2022-10-23 00:00") & ts>as.POSIXct("2022-06-29 00:00"))), aes(x=ts))+
+  geom_line(aes(y=value), color="grey40")+
+  geom_line(aes(y=gro_tot), color = "seagreen")+
+  geom_line(aes(y=twd), color ="red")
 
 #plotting 140
 ggplot((subset(d140, ts < as.POSIXct("2022-10-23 00:00") & ts > as.POSIXct("2022-06-29 00:00"))), aes(x=ts))+
@@ -27,10 +31,29 @@ plot140<-ggplot((subset(d140, ts < as.POSIXct("2022-10-23 00:00") & ts > as.POSI
                  geom_line(aes(y=twd), color = "red")
                  
 #labeling the graph 140
-print(plot140+labs(x="Date of 2022", y="Displacement of Dendrometer (um)",title="Growth, Dendrometer Displacement, and Tree Water Deficit 
+print(plot140+labs(x="Date of 2022", y="Displacement Value (um)",title="Growth, Displacement Value, and Tree Water Deficit 
 of Douglas-Fir 140"))
+
 #add legend but this isn't working.
-legend('right', legend=c("Overall Growth", "Daily Shrinkage and Growth", "Tree Water Deficit"),col=c("seagreen", "grey40", "red"), lty=1)
+#option 1
+gglegend('right', legend=c("Growth", "Displacement of Dendrometer", "Tree Water Deficit (TWD)"),col=c("seagreen", "grey40", "red"), lty=1)
+
+#option 2
+colors <- c("Growth" = "seagreen", "Tree Water Deficit (TWD)" = "red", "Displacement of Dendrometer" = "grey40")
+df <- gather(d140, key = measure, value = Rate, 
+             c("Growth", "Tree Water Deficit (TWD)", "Displacement of Dendrometer"))
+ggplot((subset(d140, ts < as.POSIXct("2022-10-23 00:00") & ts > as.POSIXct("2022-06-29 00:00"))), aes(x=ts))+
+  geom_line(aes(y=value), color = "grey40")+
+  geom_line(aes(y=gro_tot), color = "seagreen")+
+  geom_line(aes(y=twd), color = "red")
+
+#option 3
+install.packages("tidyr")
+
+library(tidyr)
+df <- gather(by_year_percentage, key = measure, value = Rate, 
+             c("deathpercentage", "tamponadepercentage", "protaminepercentage"))
+
 
 #plotting 133
 ggplot((subset(d133, ts < as.POSIXct("2022-10-23 00:00") & ts > as.POSIXct("2022-06-29 00:00"))), aes(x=ts))+
@@ -43,7 +66,7 @@ plot_133<-ggplot((subset(d133, ts < as.POSIXct("2022-10-23 00:00") & ts > as.POS
   geom_line(aes(y=gro_tot), color = "seagreen")+
   geom_line(aes(y=twd), color = "red")
 
-print(plot_133+labs(x="Date of 2022", y="Displacement of Dendrometer (um)",title="Growth, Dendrometer Displacement, and Tree Water Deficit 
+print(plot_133+labs(x="Date of 2022", y="Displacement Value (um)",title="Growth, Displacement Value, and Tree Water Deficit 
 of Douglas-Fir 133"))
 
 
